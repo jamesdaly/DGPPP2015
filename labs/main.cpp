cee62d0d4f40171c2c2cde8f714adca14b550e6a
James Daly <james@atomsplitgames.com>
2015-12-09 15:58:19 +0000

    temp commit

--- labs/main.cpp
+++ labs/main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
-#include "ClassOne.h"
+#include "GameManager.h"
 
 using namespace std;
+
+//Comp
+
+
 
 void main()
 {
 
-	//instantiate a local copy of the object
-	ClassOne c1;
-
-	//create a pointer to the object
-	ClassOne* c2 = new ClassOne();
-
-	cout << "Class One Created, hidden variable currently equals:  " << c1.getHiddenVariable() << endl;
-
-	c1.setHiddenVariable(10);
-
-	cout << "Class One setter called, now equal to : " << c1.getHiddenVariable() << endl;
-
-	delete c2;
-
-	std::cin.get();
+	//waiting for gamemanager dev
+	GameManager* gm = new GameManager();
+	while(true)
+	{
+		//handle input (keystrokes, controller input, mouse input, also check for quit events e.g. someone clicking the red x on the window)
+		gm->handleInput();
+		//update AI, Physics, World caclulations, player position, collisions
+		gm->update();
+		//draw call to the drawing engine
+		gm->draw();
+	}
 
 	
-
-
 }
